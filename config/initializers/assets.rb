f92f5c6c55f9d5cbc3a07f3a915a7e41c9d283df
MichelleNonato93 <michelle.nonato@gmail.com>
2014-09-23 13:48:41 +0800

    landing for module Procurement

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@ Rails.application.config.assets.precompile += %w( templateA.css images/templateA
 Rails.application.config.assets.precompile += %w( templateB.css images/templateB/*.jpg images/templateB/*.png)
 Rails.application.config.assets.precompile += %w( templateC.css images/templateC/*.jpg images/templateC/*.png)
 Rails.application.config.assets.precompile += %w( templateD.css images/templateD/*.jpg images/templateD/*.png)
-Rails.application.config.assets.precompile += %w( module.css )
+Rails.application.config.assets.precompile += %w( module.css images/modules/*.jpg images/modules/*.png )
