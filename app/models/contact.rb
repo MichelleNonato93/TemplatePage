6c960158637d2414a0f9473e7c59be5fd174e707
MichelleNonato93 <michelle.nonato@gmail.com>
2014-10-14 10:37:15 +0800

    email uniqueness

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,15 @@
 class Contact < ActiveRecord::Base
-  validates :name,
-            :email,
-            :organization,
+  validates :organization,
             :role,
             presence: true
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: { with: /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})\z/, :multiline => true }
+
+  validates :name,
+            presence: true,
+            length:  { maximum: 100 }
+
 end
